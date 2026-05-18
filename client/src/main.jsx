5b4f3b81910596fd97537f3d95c2396a733727cb
Mattias Holmqvist <sirmathioso@gmail.com>
2026-05-18 15:58:43 +0200

    Fixed warnings and suppressed non errors

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
 
+window.addEventListener('unhandledrejection', (event) => {
+  if (event.reason?.name === 'AbortError' && event.reason?.message?.includes('media resource')) {
+    event.preventDefault();
+  }
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
